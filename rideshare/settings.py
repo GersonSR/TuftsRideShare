51cba9ddf8f739a02a611847639768084c818914
GersonSR <salmeron.gerson@gmail.com>
2017-03-28 12:27:17 -0400

    Link Changes.

--- rideshare/settings.py
+++ rideshare/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['tuftsrideshare.herokuapp.com', 'unirideshare.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
